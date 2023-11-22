0dc7e0461b3bac024edc36da83714bf9f0f4420c
Gunjan Paul <gunjanpaul99.9@gmail.com>
2023-11-23 03:48:57 +0530

    Order page done

--- lib/screens/admin/order_history.dart
+++ lib/screens/admin/order_history.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class OrderHistoryScreen extends StatefulWidget {
@@ -99,10 +101,168 @@ class _OrderHistoryScreenState extends State<OrderHistoryScreen> {
               itemBuilder: (context, index) {
                 DocumentSnapshot ds = snapshot.data!.docs[index];
                 print(ds);
+                String name = '';
+                for (var i = 0; i < ds['items'].length; i++) {
+                  name += ds['items'][i]['title'] +
+                      ':' +
+                      ds['items'][i]['quantity'].toString();
+                  if (i != ds['items'].length - 1) {
+                    name += ', ';
+                  }
+                }
+                bool paid = ds['paid'];
                 return Card(
-                  child: ListTile(
-                    title: Text('${ds.id}'),
-                    subtitle: Text('${ds['total']}'),
+                  child: InkWell(
+                    onTap: () {
+                      //show a modal sheet containing the order details
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) => Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.5,
+                                child: Column(
+                                  children: [
+                                    ListTile(
+                                      title: Text('Order ID: ${ds.id}'),
+                                      subtitle: Text(
+                                          'Time: ${ds['time'].toDate().toString()}'),
+                                    ),
+                                    Expanded(
+                                      child: ListView.builder(
+                                        itemCount: ds['items'].length,
+                                        itemBuilder: (context, index) {
+                                          return ListTile(
+                                            title: Text(
+                                                ds['items'][index]['title']),
+                                            subtitle: Text(ds['items'][index]
+                                                    ['quantity']
+                                                .toString()),
+                                            trailing: Text(
+                                                '₹${(ds['items'][index]['price'] * ds['items'][index]['quantity']).toStringAsFixed(2)}'),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                    ListTile(
+                                      title: Text('Total'),
+                                      trailing: Text(
+                                          '₹${ds['total'].toStringAsFixed(2)}'),
+                                    ),
+                                    ListTile(
+                                      title: Text('Payment Status'),
+                                      trailing: Text(
+                                          ds['paid'] ? 'Paid' : 'Not Paid'),
+                                    ),
+                                    // ListTile(
+                                    //   title: Text('Customer Name'),
+                                    //   trailing: Text(ds['name']),
+                                    // ),
+                                    // ListTile(
+                                    //   title: Text('Customer Phone'),
+                                    //   trailing: Text(ds['phone']),
+                                    // ),
+                                    // ListTile(
+                                    //   title: Text('Customer Address'),
+                                    //   trailing: Text(ds['address']),
+                                    // ),
+                                  ],
+                                ),
+                              ));
+                    },
+                    child: ListTile(
+                      title: Expanded(
+                        child: Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              child: Text(
+                                name,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      subtitle: Text('${ds['total']}'),
+                      leading: InkWell(
+                        onLongPress: () async {
+                          await FirebaseFirestore.instance
+                              .collection('Order')
+                              .doc(ds.id)
+                              .update({'paid': !paid}).then((_) {
+                            //show a snackbar
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Order Payment status updated'),
+                              ),
+                            );
+                          });
+                        },
+                        child: CircleAvatar(
+                          backgroundColor: paid ? Colors.green : Colors.red,
+                          child: Icon(
+                            paid ? Icons.paid : Icons.warning_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      trailing: //iconbutton that deletes the order, after a confirmation from a dialog box
+                          IconButton(
+                        onPressed: () async {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              buttonPadding: EdgeInsets.zero,
+                              // contentPadding: EdgeInsets.zero,
+                              title: const Text('Delete Order'),
+                              content: Text(
+                                  'Are you sure you want to delete this order?\nID: ${ds.id}]'),
+                              actions: [
+                                TextButton(
+                                  style: ButtonStyle(
+                                    foregroundColor: MaterialStateProperty.all(
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                                  ),
+                                  onPressed: () async {
+                                    await FirebaseFirestore.instance
+                                        .collection('Order')
+                                        .doc(ds.id)
+                                        .delete()
+                                        .then((_) {
+                                      //show a snackbar
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        const SnackBar(
+                                          content: Text('Order deleted'),
+                                        ),
+                                      );
+                                    });
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Yes'),
+                                ),
+                                TextButton(
+                                  style: ButtonStyle(
+                                    foregroundColor: MaterialStateProperty.all(
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .onSecondary),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('No'),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                      ),
+                    ),
                   ),
                 );
               },
